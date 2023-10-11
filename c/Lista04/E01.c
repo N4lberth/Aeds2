412f665b4c66fb8b3f1cb45b4343a67e9c2f356c
Nalberth Henrique Vieira Pinto <nalberthvieira@gmail.com>
2023-10-10 21:21:06 -0300

    .

--- c/Lista04/E01.c
+++ c/Lista04/E01.c
@@ -4,18 +4,24 @@ int main(void)
 {
     int V[10], n;
 
+ //pede o primeiro numero ao usuario
     printf("digite o 1 numero ");
     scanf("%d", &V[0]);
 
+ //salva o primeiro numero em uma variavel 
     n=V[0];
 
+ //extruturra de repetição
     for(int i=1; i<10; i++){
+     //pede os outros numeros ao usuario e testa qual o maior entre eles
         printf("digite o %d numero ", i+1);
         scanf("%d", &V[i]);
 
         if(n<V[i])
+        {
             n=V[i];
-    }
+        }//end if
+    }//end for
 
     printf("%d eh o maior numero", n);
 
--- c/Lista04/E03.c
+++ c/Lista04/E03.c
@@ -4,11 +4,13 @@ int main(void)
 {
     int v[10];
 
+ //extrutura de repetição para pedir ao usuario os numeros
     for(int i=0; i<10; i++){
         printf("digite o %d numero ", i);
         scanf("%d", &v[i]);
     }
-    
+
+ //extrutura de repetição para printar todos os numeros do vetor na tela  
     for(int i=0; i<10; i++){
         printf("%d, ", v[i]);
     }
