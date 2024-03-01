45b3071206791646889d56fd164d694300b8577a
Nalberth Henrique Vieira Pinto <nalberthvieira@gmail.com>
2024-02-29 21:56:57 -0300

    nao gosto de java

--- Aeds2/java/Tp01/E01.java
+++ Aeds2/java/Tp01/E01.java
@@ -1,5 +1,33 @@
-package Tp01;
+import java.util.Scanner;
 
-public  class E01{
-    
+public class E01 {
+
+    public static Boolean palindromo(String texto){
+        
+        String temp = "";
+
+        for(int i = texto.length() - 1; i >= 0 ; i--){
+            temp = temp + texto.charAt(i);
+        }
+        if(texto.equals(temp)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        String texto;
+
+        while (!(texto=scanner.nextLine()).equals("FIM")) {
+            if(palindromo(texto)){
+                System.out.println("SIM");
+            }else{
+                System.out.println("NAO");
+            }
+        }
+        scanner.close();
+    }
 }
