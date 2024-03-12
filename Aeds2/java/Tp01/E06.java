fac843abdac5d2d4b75161ca9c78b8398cd8df69
Nalberth Henrique Vieira Pinto <nalberthvieira@gmail.com>
2024-03-12 19:01:15 -0300

    kkkkkkkkkk

---
+++ Aeds2/java/Tp01/E06.java
@@ -0,0 +1,68 @@
+public class E06 {
+    
+    public static boolean somenteVogal(String texto){
+        for(int i = 0; i < texto.length(); i++){
+            char temp = texto.charAt(i);
+            if(temp != 'a' && temp != 'e' && temp != 'i' && temp != 'o' && temp != 'u' && temp != 'A' && temp != 'E' && temp != 'I' && temp != 'O' && temp != 'U'){
+                return false;  
+            }         
+        }
+        return true;
+    }
+
+    public static boolean somenteConsoante(String texto){
+        for(int i = 0; i < texto.length(); i++){
+            char temp = texto.charAt(i);
+            if(!Character.isLetter(temp) || temp == 'a' || temp == 'e' || temp == 'i' || temp == 'o' || temp == 'u' || temp == 'A' || temp == 'E' || temp == 'I' || temp == 'O' || temp == 'U'){
+                return false;  
+            }                  
+        }
+        return true;
+    }
+
+    public static boolean numeroInteiro(String texto){
+        if(texto.matches("\\d+")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean numeroReal(String texto){
+        if(texto.matches("^[-+]?[0-9]*([.,][0-9]+)?$") && !texto.matches("\\d+")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        String texto;
+
+        while (!(texto=MyIO.readLine()).equals("FIM")) {
+            
+            if(somenteVogal(texto)){
+                MyIO.println("SIM");
+            } else {
+                MyIO.println("NAO");
+            }
+            if(somenteConsoante(texto)){
+                MyIO.println("SIM");
+            } else {
+                MyIO.println("NAO");
+            }
+            if(numeroInteiro(texto)){
+                MyIO.println("SIM");
+            } else {
+                MyIO.println("NAO");
+            }
+            if(numeroReal(texto)){
+                MyIO.println("SIM");
+            } else {
+                MyIO.println("NAO");
+            }
+        }
+       
+    }
+}
